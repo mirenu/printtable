0e5b38c617b3ca2f3c555b023b2c53b5344746cc
mirenu <55189788+mirenu@users.noreply.github.com>
2019-09-11 14:53:24 +0200

    Powetable
    Powertable

--- printtable.py
+++ printtable.py
@@ -1,5 +1,13 @@
-def multtable(number, limit):
-    for i in range(1, limit+1):
-        print(i * number)
+#def multtable(number, limit):
+#    for i in range(1, limit+1):
+#        print(i * number)
 
-multtable(7, 10)
+#multtable(7, 10)
+
+def powertable(number, limit):
+	for i in range (1, limit+1):
+		print (i**number)
+ 
+
+powertable(2,4)
+powertable(3,3)
